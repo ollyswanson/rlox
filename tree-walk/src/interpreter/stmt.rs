cc7ecd9902de3c66ec0f9fb148e37ad6440a0562
Olly Swanson <olly.swanson95@gmail.com>
2022-06-09 16:23:06 +0100

    Interpret conditional statments

--- tree-walk/src/interpreter/stmt.rs
+++ tree-walk/src/interpreter/stmt.rs
@@ -1,6 +1,4 @@
-use lox_syntax::ast::expr::Expr;
-use lox_syntax::ast::stmt::Stmt;
-use lox_syntax::Identifier;
+use lox_syntax::ast::stmt::{Block, ExprStmt, If, Print, Stmt, Var};
 
 use crate::interpreter::error::RResult;
 
@@ -11,39 +9,51 @@ impl Interpreter {
         use Stmt::*;
 
         match stmt {
-            Var(v) => self.execute_var_stmt(&v.id, &v.expr),
-            Print(p) => self.execute_print(&p.expr),
-            Expr(s) => self.execute_expr_statement(&s.expr),
-            Block(b) => self.execute_block_stmt(&b.stmts),
-            If(i) => todo!(),
+            Var(v) => self.execute_var_stmt(v),
+            Print(p) => self.execute_print(p),
+            Expr(s) => self.execute_expr_statement(s),
+            Block(b) => self.execute_block_stmt(b),
+            If(i) => self.execute_if_stmt(i),
         }
     }
 
-    fn execute_block_stmt(&mut self, stmts: &[Stmt]) -> RResult<()> {
+    fn execute_block_stmt(&mut self, block: &Block) -> RResult<()> {
         self.scoped_statement(|i| {
-            for stmt in stmts {
+            for stmt in block.stmts.iter() {
                 i.execute_stmt(stmt)?
             }
             Ok(())
         })
     }
 
-    fn execute_var_stmt(&mut self, id: &Identifier, expr: &Expr) -> RResult<()> {
-        let value = self.evaluate_expr(expr)?;
-        self.environment.define(&id.name, value);
+    fn execute_if_stmt(&mut self, if_stmt: &If) -> RResult<()> {
+        if self.evaluate_expr(&if_stmt.cond)?.is_truthy() {
+            self.execute_stmt(&if_stmt.then_stmt)
+        } else {
+            if_stmt
+                .else_stmt
+                .as_deref()
+                .map(|else_stmt| self.execute_stmt(else_stmt))
+                .unwrap_or(Ok(()))
+        }
+    }
+
+    fn execute_var_stmt(&mut self, var: &Var) -> RResult<()> {
+        let value = self.evaluate_expr(&var.expr)?;
+        self.environment.define(&var.id.name, value);
         Ok(())
     }
 
-    fn execute_print(&mut self, expr: &Expr) -> RResult<()> {
-        let value = self.evaluate_expr(expr)?;
+    fn execute_print(&mut self, print: &Print) -> RResult<()> {
+        let value = self.evaluate_expr(&print.expr)?;
         // Should probably be replaced with something that passes value to a Printer rather
         // than printing to stdout directly
         println!("{}", value);
         Ok(())
     }
 
-    fn execute_expr_statement(&mut self, expr: &Expr) -> RResult<()> {
-        self.evaluate_expr(expr)?;
+    fn execute_expr_statement(&mut self, expr_stmt: &ExprStmt) -> RResult<()> {
+        self.evaluate_expr(&expr_stmt.expr)?;
         Ok(())
     }
 
